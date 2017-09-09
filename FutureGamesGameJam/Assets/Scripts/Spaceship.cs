64316742e5b0c0445d3ac97620901627e40e6b4d
Tobias Nylin <tobnyl@gmail.com>
2017-09-09 12:15:59 +0200

    Dummy movement

---
+++ FutureGamesGameJam/Assets/Scripts/Spaceship.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spaceship : MonoBehaviour 
+{
+    #region Fields/Properties
+
+    private Rigidbody _rigidbody;
+	
+    private Vector2 AxisLeft
+    {
+        get { return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical") * -1); }
+    }
+
+	#endregion
+	#region Events
+	
+	void Awake()
+	{
+        _rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	void Start() 
+	{
+		
+	}
+
+	void Update() 
+	{
+		
+	}
+
+    void FixedUpdate()
+    {
+
+        Debug.LogFormat("Axis: {0}", AxisLeft);
+
+        _rigidbody.AddForce(transform.forward * AxisLeft.x);
+        _rigidbody.AddForce(transform.right * AxisLeft.y);
+
+    }
+
+    #endregion
+    #region Methods
+
+
+
+    #endregion
+}
